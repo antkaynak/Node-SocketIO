46e6dc7dbe69e05d689f984057d165775ef371ec
Ant Kaynak <antkaynak1@gmail.com>
2018-06-21 19:24:26 +0300

    added data structure to store user details

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,39 @@
 
+const getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
 //no need to specify the url to host
 //socketio will try to conncet to the server it is hosted from
 const socket = io(); 
 socket.on('connect', function() {
     console.log('Connected to server.');
+
+    const name = getUrlParameter('name');
+    const room = getUrlParameter('room');
+
+    console.log(name);
+    console.log(room);
+
+    //join a room
+    socket.emit('join', { name, room}, function(error){
+        if(error){
+           alert('An error occurred!', error);
+           return window.location.href = '/';
+        }
+        console.log('Success!');
+    });
 
     //socket.emit('createMessage',{
     //    to:'example@com',
@@ -15,6 +45,10 @@ socket.on('connect', function() {
 
 socket.on('disconnect',function() {
     console.log('Disconnected from server.');
+});
+
+socket.on('updateUserList', function(users){
+    console.log(users);
 });
 
 socket.on('newMessage', function(message){
--- server/server.js
+++ server/server.js
@@ -5,11 +5,16 @@ const express = require('express');
 const socketIO = require('socket.io');
 const moment = require('moment');
 
+const {User} = require('../server/users');
+const {ActiveUserList} = require('../server/users');
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app); //using http server instead of express server
 const io = socketIO(server);
+
+const list = new ActiveUserList();
 
 
 
@@ -24,20 +29,47 @@ io.on('connection', (socket) => {
     //    createdAt: new Date().toString()
     //});
 
-    socket.emit('newMessage',{
-        from: 'Chat App',
-        text: 'Welcome',
-        createdAt: moment().valueOf()
-    });
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Chat App',
-        text: 'A new user joined!',
-        creatdAt: moment().valueOf()
-    })
+
+    socket.on('join', (params, callback)=>{
+        if(typeof params.name !== 'string' || typeof params.room !== 'string' 
+        || params.name.trim().length === 0 || params.room.trim().length === 0){
+            return callback('Required params! *name *room');
+        }
+
+        socket.join(params.room);
+        list.removeUser(socket.id);
+        list.addUser(new User(socket.id,params.name,params.room));
+
+        //not using broadcast because we want to send the list to the newly connected user as well.
+        io.to(params.room).emit('updateUserList', list.getUserList(params.room));
+
+        socket.emit('newMessage',{
+            from: 'Chat App',
+            text: 'Welcome',
+            createdAt: moment().valueOf()
+        });
+
+        socket.broadcast.to(params.room).emit('newMessage', {
+            from: `${params.room}`,
+            text: `A new user ${params.name} has joined!`,
+            createdAt: moment().valueOf()
+        })
+
+    
+    });
 
     socket.on('disconnect', () => {
         console.log('An user was disconnected');
+        const user = list.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList', list.getUserList(params.room));
+            io.to(user.room).emit('newMessage',{
+                from: `${params.room}`,
+                text: `User ${params.name} left!`,
+                createdAt: moment().valueOf()
+            });
+        }
     });
 
     socket.on('createMessage', (message, callback)=>{
@@ -49,7 +81,7 @@ io.on('connection', (socket) => {
         //});
 
         //send to all but this socket
-        socket.broadcast.emit('newMessage', {
+        socket.broadcast.to(list.getUser(socket.id).room).emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: moment().valueOf()
